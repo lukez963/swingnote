d50558f6182d86db2a756a5c87df7e81ba76b8bd
Luke Zhang <lukezhang963@gmail.com>
2024-07-16 22:30:14 -0400

    removed unused imports from 2 classes

--- src/main/java/com/tool/NoteData.java
+++ src/main/java/com/tool/NoteData.java
@@ -1,8 +1,6 @@
 package com.tool;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
--- src/main/java/com/tool/ToolApplication.java
+++ src/main/java/com/tool/ToolApplication.java
@@ -2,15 +2,8 @@ package com.tool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.swing.*;
-import java.awt.BorderLayout;
 
 @SpringBootApplication
 public class ToolApplication {
